d5b44e85ed67eaa4c4e8340ccd7e814bbf07acfe
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-01 14:12:55 -0500

    Add dinamic functionality for add book and autor.

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const form = document.getElementsByTagName('form');
 const bookTitle = document.getElementById('book-title');
 const authorName =document.getElementById('book-author');
 const button = document.getElementById('submit')
+const listDisplay = document.querySelector('#book-list');
 
-let name = "";
-let book = "";
+let array = new Array();
+let counter = 0;
 
-button.addEventListener('click', function (e) {
-  name = form.author.value;
-  book = form.title.value;
-
-  console.log(name);
+button.addEventListener('click', (e) => {
   e.preventDefault();
-});
-console.log("text")
+  const book = bookTitle.value;
+  const author = authorName.value;
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+  const removebtn = document.createElement('button');
+  array.push(`${book} by ${author} `)
+  li.textContent = `${array[counter]}`;
+  removebtn.textContent = 'Remove';
+  li.appendChild(removebtn);
+  ul.appendChild(li);
+  listDisplay.appendChild(ul);
+  counter++;
+})
+
