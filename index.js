b2586acddecfbaef34fe2550a9d29a3a91e92e10
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-01 14:46:11 -0500

    Fix error where `ul` was creating on click

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+/* eslint-disable semi */
 const form = document.getElementsByTagName('form');
 const bookTitle = document.getElementById('book-title');
 const authorName =document.getElementById('book-author');
-const button = document.getElementById('submit')
-const listDisplay = document.querySelector('#book-list');
+const button = document.getElementById('submit');
+const list = document.querySelector('#books-ul');
+
 
 let array = new Array();
 let counter = 0;
@@ -11,15 +13,15 @@ button.addEventListener('click', (e) => {
   e.preventDefault();
   const book = bookTitle.value;
   const author = authorName.value;
-  const ul = document.createElement('ul');
   const li = document.createElement('li');
   const removebtn = document.createElement('button');
   array.push(`${book} by ${author} `)
   li.textContent = `${array[counter]}`;
+  removebtn.className = 'removebuttons'
   removebtn.textContent = 'Remove';
+  removebtn.setAttribute('id', `removeid_${counter}`);
   li.appendChild(removebtn);
-  ul.appendChild(li);
-  listDisplay.appendChild(ul);
+  list.appendChild(li);
   counter++;
 })
 
