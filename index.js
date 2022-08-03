690de4eec645c8c304c1c4b9fec5e904eac9c1ea
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-02 23:52:44 -0500

    Fix ESLint errors

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ let delcounter = 0;
 
 const dataform = () => {
   localStorage.setItem('data', JSON.stringify(array));
-  console.log(`testing ${counter}`);
 };
 
 const removeBook = (e) => {
   e.preventDefault();
-  console.log(array);
-  console.log(delcounter)
   const elid = e.target.parentNode.id
   const remotion = e.target.parentNode;
   const localcount = (elid - delcounter);
@@ -51,7 +48,6 @@ button.addEventListener('click', (e) => {
   dataform();
   // COUNTER-TRACKER
   counter += 1;
-  console.log(array);
 })
 
 const mystorage = JSON.parse(localStorage.getItem('data').split(','));
@@ -71,7 +67,6 @@ document.addEventListener('DOMContentLoaded', () => {
     removebtn.addEventListener('click', removeBook);
     li.appendChild(removebtn);
     list.appendChild(li);
-    counter++;
-  }
-  )
+    delcounter += 1;
+  }),
 })
