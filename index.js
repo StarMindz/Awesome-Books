a9688dc49b19645ce4417b0114701d0cc7e59959
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-08-03 07:22:34 +0100

    Add a class with add, display and remove functions

--- index.js
+++ index.js
@@ -3,55 +3,74 @@ const bookTitle = document.getElementById('book-title');
 const authorName = document.getElementById('book-author');
 const button = document.getElementById('submit');
 const list = document.querySelector('#books-ul');
-const array = [];
-let counter = 0;
-let delcounter = 0;
+// const array = [];
+// let counter = 0;
+// let delcounter = 0;
+
+class Collection {
+  constructor() {
+    this.bookArray = [];
+    this.counter = 0;
+    this.delcounter = 0;
+  }
+  
+  addBook(book, author) {
+    const object = {};
+    object.id = this.counter;
+    object.title = book;
+    object.author = author;
+    this.bookArray.push(object);
+  }
+  
+  display(book, author) {
+    const li = document.createElement('li');
+    const removebtn = document.createElement('button');
+    li.setAttribute('id', `${this.counter}`);
+    li.textContent = `${book} by ${author}`;
+    // REMOVE BUTTON
+    removebtn.className = 'removebuttons'
+    removebtn.textContent = 'Remove';
+    removebtn.addEventListener('click', this.removeBook);
+    li.appendChild(removebtn);
+    list.appendChild(li);
+  }
+
+  removeBook (e) {
+    e.preventDefault();
+    const elid = e.target.parentNode.id
+    const remotion = e.target.parentNode;
+    const localcount = (elid - this.delcounter);
+    list.removeChild(remotion);
+    array.splice(localcount, 1);
+    this.delcounter += 1;
+    dataform();
+  }
+}
+
+const books = new Collection();
 
 const dataform = () => {
-  localStorage.setItem('data', JSON.stringify(array));
+  localStorage.setItem('data', JSON.stringify(books.bookArray));
 };
-
-const removeBook = (e) => {
-  e.preventDefault();
-  const elid = e.target.parentNode.id
-  const remotion = e.target.parentNode;
-  const localcount = (elid - delcounter);
-  list.removeChild(remotion);
-  array.splice(localcount, 1);
-  delcounter += 1;
-  dataform();
-}
 
 button.addEventListener('click', (e) => {
   e.preventDefault();
   const book = bookTitle.value;
   const author = authorName.value;
-  const li = document.createElement('li');
-  const removebtn = document.createElement('button');
-  const object = {};
-  object.id = counter;
-  object.title = book;
-  object.author = author;
-  array.push(object);
-  li.setAttribute('id', `${counter}`);
-  li.textContent = `${book} by ${author}`;
-  // REMOVE BUTTON
-  removebtn.className = 'removebuttons'
-  removebtn.textContent = 'Remove';
-  removebtn.addEventListener('click', removeBook);
-  li.appendChild(removebtn);
-  list.appendChild(li);
+  books.addBook(book, author);
+  books.display(book, author);
   // RESET-VALUES
   authorName.value = '';
   bookTitle.value = '';
   // LOCAL-STORAGE
   dataform();
   // COUNTER-TRACKER
-  counter += 1;
+  books.counter += 1;
 })
 
 const mystorage = JSON.parse(localStorage.getItem('data').split(','));
 document.addEventListener('DOMContentLoaded', () => {
+  console.log(mystorage);
   mystorage.forEach((element) => {
     const li = document.createElement('li');
     const removebtn = document.createElement('button');
