15d4b902c3ad442928a50e6d2d14eaa4cf5334d6
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-03 09:36:12 -0500

    Fix ESlint errors, deletes unused comments.

--- index.js
+++ index.js
@@ -3,27 +3,23 @@ const bookTitle = document.getElementById('book-title');
 const authorName = document.getElementById('book-author');
 const button = document.getElementById('submit');
 const list = document.querySelector('#books-ul');
-// const array = [];
-// let counter = 0;
-// let delcounter = 0;
 
 class Collection {
-  constructor() {
+  constructor () {
     this.bookArray = [];
     this.counter = 0;
     this.delcounter = 0;
   }
-  
-  addBook(book, author) {
+
+  addBook (book, author) {
     const object = {};
     object.id = this.counter;
     object.title = book;
     object.author = author;
     this.bookArray.push(object);
-    // console.log(this.bookArray);
   }
-  
-  display(book, author) {
+
+  display (book, author) {
     const li = document.createElement('li');
     const removebtn = document.createElement('button');
     li.setAttribute('id', `${this.counter}`);
@@ -74,8 +70,8 @@ const mystorage = JSON.parse(localStorage.getItem('data').split(','));
 document.addEventListener('DOMContentLoaded', () => {
   console.log(mystorage);
   mystorage.forEach((element) => {
-    let book = element.title;
-    let author = element.author;
+    const book = element.title;
+    const author = element.author;
     books.addBook(book, author);
     books.display(book, author);
     books.counter += 1;
